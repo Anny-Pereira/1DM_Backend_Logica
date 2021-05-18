ff74777b877d4e8176d1869ec54959443b05c1b9
Anny-Pereira <isabelle.pereira04@gmail.com>
2021-05-18 20:02:22 -0300

    teste_POO

--- teste_POO/Program.cs
+++ teste_POO/Program.cs
@@ -6,6 +6,8 @@ namespace teste_POO
     {
         static void Main(string[] args)
         {
+            bool JogarNovamente = false;
+
             personagem primeiroPersonagem = new personagem();
 
             primeiroPersonagem.pontoVida = 100;
@@ -19,7 +21,7 @@ namespace teste_POO
             Console.Write("Qual a habilidade do personagem? ");
             primeiroPersonagem.habilidade = Console.ReadLine();
 
-            Console.WriteLine($"Os pontos de vida deste personagem é de {primeiroPersonagem.pontoVida}");
+            Console.WriteLine($"\nOs pontos de vida deste personagem é de {primeiroPersonagem.pontoVida}\n\n");
 
 
             personagem segundoPersonagem = new personagem();
@@ -35,48 +37,73 @@ namespace teste_POO
             Console.Write("Qual a habilidade do personagem? ");
             segundoPersonagem.habilidade = Console.ReadLine();
 
-            Console.WriteLine($"Os pontos de vida deste personagem é de {segundoPersonagem.pontoVida}");
+            Console.WriteLine($"\nOs pontos de vida deste personagem é de {segundoPersonagem.pontoVida}\n\n");
 
-            Console.WriteLine("\nSelecione dentre as opções:\n");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($@"
+            do
+            {
+
+                Console.WriteLine($"\nSelecione para {primeiroPersonagem.nome}:");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($@"
     =====================
     | 1- Atacar         |
     | 2- Defender       |
     =====================
             ");
-            Console.ForegroundColor = ConsoleColor.White;
-            string opçãoPrimeiroPersonagem = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                string opçãoPrimeiroPersonagem = Console.ReadLine();
 
-            Console.WriteLine("\nSelecione dentre as opções:\n");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($@"
+                Console.WriteLine($"\nSelecione para {segundoPersonagem.nome}:");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($@"
     =====================
     | 1- Atacar         |
     | 2- Defender       |
     =====================
             ");
+                Console.ForegroundColor = ConsoleColor.White;
+                string opçãoSegundoPersonagem = Console.ReadLine();
+
+                if (opçãoPrimeiroPersonagem == "1" && opçãoSegundoPersonagem == "1")
+                {
+                    primeiroPersonagem.pontoVida = primeiroPersonagem.pontoVida - segundoPersonagem.Atacar();
+                    segundoPersonagem.pontoVida = segundoPersonagem.pontoVida - primeiroPersonagem.Atacar();
+                }
+                else if (opçãoPrimeiroPersonagem == "1" && opçãoSegundoPersonagem == "2")
+                {
+                    segundoPersonagem.pontoVida = segundoPersonagem.pontoVida - (primeiroPersonagem.Atacar() - segundoPersonagem.Defender());
+                }
+                else if (opçãoPrimeiroPersonagem == "2" && opçãoSegundoPersonagem == "1")
+                {
+                    primeiroPersonagem.pontoVida = primeiroPersonagem.pontoVida - (segundoPersonagem.Atacar() - primeiroPersonagem.Defender());
+                }
+                else if (opçãoPrimeiroPersonagem == "2" && opçãoSegundoPersonagem == "2") { }
+
+                Console.WriteLine($"Os pontos de vida do {primeiroPersonagem.nome} é de {primeiroPersonagem.pontoVida}");
+                Console.WriteLine($"Os pontos de vida do {segundoPersonagem.nome} é de {segundoPersonagem.pontoVida}");
+
+                Console.WriteLine($"\n\nDeseja jogar novamente (s-sim / n-não)?");
+                string RespostaNovamente = Console.ReadLine();
+
+                if (RespostaNovamente == "s")
+                {
+                    JogarNovamente = true;
+                }
+                else if (RespostaNovamente == "n")
+                {
+                    JogarNovamente = false;
+                }
+
+                if (primeiroPersonagem.pontoVida == 0 && segundoPersonagem.pontoVida == 0)
+                {
+                    JogarNovamente = false;
+                }
+            }
+            while (JogarNovamente);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\n\nFIM DE JOGO!");
             Console.ForegroundColor = ConsoleColor.White;
-            string opçãoSegundoPersonagem = Console.ReadLine();
-            
-            if (opçãoPrimeiroPersonagem == "1" && opçãoSegundoPersonagem == "1")
-            {
-                primeiroPersonagem.pontoVida = primeiroPersonagem.pontoVida - segundoPersonagem.Atacar();
-                segundoPersonagem.pontoVida = segundoPersonagem.pontoVida - primeiroPersonagem.Atacar();
-            }
-            else if (opçãoPrimeiroPersonagem == "1" && opçãoSegundoPersonagem == "2")
-            {
-                segundoPersonagem.pontoVida = segundoPersonagem.pontoVida - (segundoPersonagem.Atacar() - primeiroPersonagem.Defender());
-            }
-            else if (opçãoPrimeiroPersonagem == "2" && opçãoSegundoPersonagem == "1")
-            {
-                primeiroPersonagem.pontoVida = primeiroPersonagem.pontoVida - (primeiroPersonagem.Atacar() - segundoPersonagem.Defender());
-            }
-            else if (opçãoPrimeiroPersonagem == "2" && opçãoSegundoPersonagem == "2") {}
-
-            Console.WriteLine($"Os pontos de vida do {primeiroPersonagem.nome} é de {primeiroPersonagem.pontoVida}");
-            Console.WriteLine($"Os pontos de vida do {segundoPersonagem.nome} é de {segundoPersonagem.pontoVida}");
-
         }
-    }
+}
 }
